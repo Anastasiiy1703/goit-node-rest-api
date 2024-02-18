4b33a5ba275a322a9c6c089ed990563b69c1974e
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-02-18 16:35:37 +0200

    commit3

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,77 @@
-import contactsService from "../services/contactsServices.js";
+const {listContacts,getContactById, addContact,removeContact, updateOneContact } = require("../services/contactsServices")
 
-export const getAllContacts = (req, res) => {};
 
-export const getOneContact = (req, res) => {};
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const deleteContact = (req, res) => {};
+ const getOneContact = async (req, res) => {
+  const contactId = req.params.id;
+  try {
+    const contact = await getContactById(contactId);
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const createContact = (req, res) => {};
+ const deleteContact = async (req, res) => {
+  const contactId = req.params.id;
+  try {
+    const deletedContact = await removeContact(contactId);
+    if (deletedContact) {
+      res.status(200).json(deletedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const updateContact = (req, res) => {};
+ const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  try {
+    const newContact = await addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const updateContact = async (req, res) => {
+  const contactId = req.params.id;
+  const updatedData = req.body;
+  try {
+    const updatedContact = await updateOneContact(contactId, updatedData);
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact
+};
