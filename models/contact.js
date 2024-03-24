6db60e584e3b53ba1940f03d33a995241c110fe1
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-03-24 17:13:18 +0200

    commit1

---
+++ models/contact.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      match: /(\d{10}|\(\d{3}\) \d{3}-\d{4})/,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  { versionKey: false }
+);
+
+export const contacts = mongoose.model("contacts", contactsSchema);
---
+++ services/contactsServices.js
@@ -0,0 +1,97 @@
+import { contacts } from "../models/contact.js";
+
+async function listContacts({ user, favorite, page, limit }) {
+  const owner = user.id;
+  if (favorite) {
+    return await contacts.find({ owner, favorite });
+  }
+  const skip = (page - 1) * limit || 0;
+  return await contacts.find({ owner }).skip(skip).limit(limit);
+}
+
+async function getContactById(contactId, owner) {
+  const contact = await contacts.findOne({ owner: owner.id, _id: contactId });
+  if (!contact || contact.length === 0) {
+    return null;
+  }
+
+  return contact;
+}
+
+async function removeContact(contactId, owner) {
+  const deletedContact = await contacts.findOneAndDelete({
+    _id: contactId,
+    owner: owner.id,
+  });
+  if (deletedContact === null) {
+    return null;
+  }
+  return deletedContact;
+}
+
+async function addContact({ name, email, phone, favorite }, owner) {
+  const newContact = await contacts.create({
+    name,
+    email,
+    phone,
+    favorite,
+    owner: owner.id,
+  });
+
+  return newContact;
+}
+
+async function updateContact(
+  contactId,
+  { id, name, email, phone, favorite },
+  owner
+) {
+  console.log(owner.id);
+  const newContact = {
+    id,
+    name,
+    email,
+    phone,
+    favorite,
+  };
+  const updatedContact = await contacts.findOneAndUpdate(
+    { _id: contactId, owner: owner.id },
+    newContact,
+    {
+      new: true,
+    }
+  );
+  if (updatedContact !== null) {
+    return updatedContact;
+  } else {
+    return null;
+  }
+}
+
+async function updateStatusContact(contactId, { favorite }, owner) {
+  const newContact = {
+    favorite,
+  };
+
+  const updatedContact = await contacts.findOneAndUpdate(
+    { _id: contactId, owner: owner.id },
+    newContact,
+    {
+      new: true,
+    }
+  );
+  if (updatedContact !== null) {
+    return updatedContact;
+  } else {
+    return null;
+  }
+}
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact,
+};
